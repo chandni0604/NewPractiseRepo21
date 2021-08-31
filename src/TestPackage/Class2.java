62bfafc0f785675a27d9713160aa59ee4111a024
Chandni Fatehchandani <chandnif6@gmail.com>
2021-08-31 21:20:12 +0530

    CLass 2 file

---
+++ src/TestPackage/Class2.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+public class Class2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Chandani Fatehchandani";
+		System.out.println("My Full name is "+ name);
+	}
+}
