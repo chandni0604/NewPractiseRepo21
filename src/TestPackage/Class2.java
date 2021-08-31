2758b6b5eaa082932ab7ff6a92956406ef1acffc
Chandni Fatehchandani <chandnif6@gmail.com>
2021-08-31 21:28:25 +0530

    Resolving conflicts

--- src/TestPackage/Class2.java
+++ src/TestPackage/Class2.java
@@ -6,6 +6,6 @@ public class Class2 {
 		// TODO Auto-generated method stub
 		String name = "Chandani Fatehchandani";
 		System.out.println("My Full name is "+ name);
-		System.out.println("Testing Conflicts scenario using git")
+		System.out.println("Testing Conflicts scenario using git");
 	}
 }
