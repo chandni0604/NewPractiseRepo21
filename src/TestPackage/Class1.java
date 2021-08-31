4689b628558f6c1932ea3b3f7c9bc64dafe0158b
Chandni Fatehchandani <chandnif6@gmail.com>
2021-08-31 20:28:53 +0530

    Changes to the same program from Branch B

--- src/TestPackage/Class1.java
+++ src/TestPackage/Class1.java
@@ -4,8 +4,9 @@ public class Class1 {
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-			String name = "Chandani";
+			String name = "Chandani6489";
 			System.out.println("My Name is "+ name);
+			System.out.println("End of my program");
 		}
 
 }
