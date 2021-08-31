162a1a7bb47f129699a09b43f86c33d522c7b4a0
Chandni Fatehchandani <chandnif6@gmail.com>
2021-08-31 08:24:38 +0530

    New Practise Repo Branch

---
+++ src/TestPackage/Class1.java
@@ -0,0 +1,12 @@
+package TestPackage;
+
+public class Class1 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			String name = "Chandani";
+			System.out.println("My Name is "+ name);
+		}
+
+}
+
